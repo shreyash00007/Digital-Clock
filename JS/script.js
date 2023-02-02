43333c277a1ac803144745c3dff397d60b856f1a
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2023-02-02 17:19:16 +0530

    Update

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+setInterval(displayclock, 500);
+
+function displayclock() {
+    var time = new Date();
+    var hrs = time.getHours();
+    var min = time.getMinutes();
+    var sec = time.getSeconds();
+
+    if (hrs > 12) {
+        hrs = hrs - 12;
+    }
+    if (hrs == 0) {
+        hrs = 12;
+    }
+    if (hrs < 10) {
+        hrs = '0' + hrs;
+    }
+    if (min < 10) {
+        min = '0' + min;
+    }
+    if (sec < 10) {
+        sec = '0' + sec;
+    }
+    document.getElementById('clock').innerHTML = hrs + ':' + min + ":" + sec;
+}
